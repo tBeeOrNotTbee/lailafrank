9abd5e472ac380268ca5bb8bd53c70a4c87f5966
Santiago Larsen <santiago.larsen93@gmail.com>
2020-02-10 11:52:36 -0300

    mi cuenta diferencia suscripcion

--- resources/views/shopMiCuenta.blade.php
+++ resources/views/shopMiCuenta.blade.php
@@ -11,7 +11,7 @@
                 <div class="card shop-bk border-0 h100x">
                     <div class="card-body d-flex flex-column justify-content-around">
                         <h5 class="card-title monserrat shop-card-title grey2 cero8em">Información de contacto</h5>
-                    <p class="card-text monserrat shop-card-text grey2 cero7em">{{Auth::user()->fullName()}}</p>
+                        <p class="card-text monserrat shop-card-text grey2 cero7em">{{Auth::user()->fullName()}}</p>
                         <p class="card-text monserrat shop-card-text grey2 cero7em">{{Auth::user()->email}}</p>
                         <hr class="w100">
                         <div class="w-100 d-flex justify-content-between">
@@ -26,11 +26,15 @@
                 <div class="card shop-bk border-0 h100x">
                     <div class="card-body d-flex flex-column justify-content-around">
                         <h5 class="card-title monserrat shop-card-title grey2 cero8em">Newsletters</h5>
-                        <p class="card-text monserrat shop-card-text grey2 cero8em">No esta suscripto al boletín</p>
-                        <hr class="w100">
-                        <div class="w-100 d-flex flex-row-reverse">
-                            <a href="shopNewsletter.php" class="shop-card-text monserrat text-uppercase grey2 cero7em"><i class="fas fa-pencil-alt"></i> Editar</a>
-                        </div>
+                        @if (in_array(Auth::user()->email, $email_list))
+                            <p class="card-text monserrat shop-card-text grey2 cero8em">Estás en nuestra lista</p>
+                        @else
+                            <p class="card-text monserrat shop-card-text grey2 cero8em">No esta suscripto al boletín</p>
+                        @endif
+                            <hr class="w100">
+                            <div class="w-100 d-flex flex-row-reverse">
+                                <a href="/shop/myaccount/newsletter" class="shop-card-text monserrat text-uppercase grey2 cero7em"><i class="fas fa-pencil-alt"></i> Editar</a>
+                            </div>
                     </div>
                 </div>
             </div>
@@ -43,24 +47,24 @@
                 <div class="card shop-bk border-0 h100x">
                     <div class="card-body d-flex flex-column justify-content-around">
                         <h5 class="card-title monserrat shop-card-title grey2 cero8em">Dirección de envío</h5>
-                        
+
                         @forelse (Auth::user()->address as $address)
-                            <p class="card-text monserrat shop-card-text grey2 cero7em my-0">{{$address->fullName()}}</p>
-                            <p class="card-text monserrat shop-card-text grey2 cero7em my-0">{{$address->cardLineOne()}}</p>
-                            <p class="card-text monserrat shop-card-text grey2 cero7em my-0">{{$address->cardLineTwo()}}</p>
-                            <p class="card-text monserrat shop-card-text grey2 cero7em my-0">{{$address->cardLineThree()}}</p>
-                            <p class="card-text monserrat shop-card-text grey2 cero7em my-0">T: {{$address->cellphone}}//{{$address->telephone ?? ''}}</p>
-                            
-                            <div class="w-100 d-flex flex-row-reverse">
-                                <a href="/shop/myaccount/addresses/edit/{{$address->id}}" class="shop-card-text monserrat text-uppercase grey2 cero7em"><i class="fas fa-pencil-alt"></i> Editar</a>
-                            </div>
-                            @break
+                        <p class="card-text monserrat shop-card-text grey2 cero7em my-0">{{$address->fullName()}}</p>
+                        <p class="card-text monserrat shop-card-text grey2 cero7em my-0">{{$address->cardLineOne()}}</p>
+                        <p class="card-text monserrat shop-card-text grey2 cero7em my-0">{{$address->cardLineTwo()}}</p>
+                        <p class="card-text monserrat shop-card-text grey2 cero7em my-0">{{$address->cardLineThree()}}</p>
+                        <p class="card-text monserrat shop-card-text grey2 cero7em my-0">T: {{$address->cellphone}}//{{$address->telephone ?? ''}}</p>
+
+                        <div class="w-100 d-flex flex-row-reverse">
+                            <a href="/shop/myaccount/addresses/edit/{{$address->id}}" class="shop-card-text monserrat text-uppercase grey2 cero7em"><i class="fas fa-pencil-alt"></i> Editar</a>
+                        </div>
+                        @break
                         @empty
-                            <p class="card-text monserrat shop-card-text grey2 cero8em">No ha establecido una dirección de envío predeterminada todavía</p>
-                            <hr class="w100">
-                            <div class="w-100 d-flex flex-row-reverse">
-                                <a href="/shop/myaccount/addresses/new" class="shop-card-text monserrat text-uppercase grey2 cero7em"><i class="fas fa-pencil-alt"></i> Nueva</a>
-                            </div>
+                        <p class="card-text monserrat shop-card-text grey2 cero8em">No ha establecido una dirección de envío predeterminada todavía</p>
+                        <hr class="w100">
+                        <div class="w-100 d-flex flex-row-reverse">
+                            <a href="/shop/myaccount/addresses/new" class="shop-card-text monserrat text-uppercase grey2 cero7em"><i class="fas fa-pencil-alt"></i> Nueva</a>
+                        </div>
                         @endforelse
                     </div>
                 </div>
