7dc5c969f92f952bc02f73b91e46e67eb1dcf34a
Santiago Larsen <santiago.larsen93@gmail.com>
2020-02-26 18:30:09 -0300

    guia de talles

--- resources/views/catalog-item.blade.php
+++ resources/views/catalog-item.blade.php
@@ -135,5 +135,30 @@
     </div>
 
 </div>
-
+  <!-- Modal -->
+  <div class="modal fade" id="tallesModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog rounded-0" id="modal-dialog" role="document">
+      <div class="modal-content rounded-0">
+        <div class="modal-header border-0">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body monserrat">
+          <a  href="{{url('img/guia_talles_desk.png')}}"><img src="{{url('img/guia_talles_desk.png')}}" alt="" class="w-100 img-fluid mb-3"></a>
+          <h2 class="monserrat-bold cero8em text-center mb-3">Elige según el talle que comúnmente usas, pero ten en cuenta lo mencionado debajo:</h2>
+          <p class="cero8em">Los calzados realizados con materiales nobles como el cuero o la cabretilla ceden con el uso.</p>
+            <p class="cero8em">En cuanto a materiales como el charol, es un material más duro que el anterior, así que si sos un medio punto en un talle, conviene selecciones el talle más alto que calces.</p>
+            <p class="cero8em">El calzado stiletto, en punta, suele ser de talle acotado en la misma, para lo cual es bueno puedas optar si sos un medio punto por el talle más alto, para que tu pie se sienta más cómodo.</p>
+            <p class="cero8em">No es lo más recomendable, seleccionar el calzado anteriormente mencionado, en lo que se denomina dedo griego (que sería que el segundo dedo del pie es más largo que el primero) dado que el mismo rozara con la punta indefectiblemente al esta estar en ángulo, para comodidad del mismo, seleccionar calzado de punta más bien redonda.</p>
+            <p class="cero8em">Si tienes un pie angosto y necesitas algo que te contenga bien el mismo para no descalzarlo sería bueno selecciones calzado que tenga tiras o cordones que crucen el empeine.</p>
+            <p class="cero8em">Si tu pie es ancho y con un empeine alto, te conviene seleccionar un calzado de horma más bien ancha para que el pie quede contenido pero cómodo.</p>
+            <p class="cero8em">Si tienes un tobillo ancho, podes optar por calzado sin tira de talón o que la misma sea bien angosta, para estilizarlo.</p>
+            <p class="cero8em">Si utilizas platillas ortopédicas en el calzado, es recomendable para tu comodidad, adquirir el talle siguiente al que calzas usualmente, así las mismas pueden ingresar con facilidad como así también tu pie y el mismo podrá estar cómodo.</p>
+            <p class="cero8em">Si usualmente soles tener la ubicación del dedo gordo, más arriba que lo normal, no selecciones calzados con punteras de charol, dado que el mismo es más rígido y va a incomodarte, puede ser que selecciones algo abierto en la punta o bien si es cerrado que sea con cuero o cabretilla que son materiales más blandos.</p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
